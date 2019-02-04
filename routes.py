bd7dce84d6a80d77f959c968514f7149f4c40f97
Saad Elkheety <saadelkheety@gmail.com>
2019-02-04 10:04:38 +0200

    sign up

--- routes.py
+++ routes.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+db.init_app(app)
+
+app.secret_key = "development-key" # to protect against a security exploit called cross sight request forgery, or CSRF.
 
 @app.route("/")
 def index():
@@ -9,6 +16,14 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if request.method == 'POST':
+        return 'Sucess!'
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
